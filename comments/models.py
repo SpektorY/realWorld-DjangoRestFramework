fbb2e981f2f1f7b44a5519d6374bfe08b891f4b0
SpektorY <165672672+SpektorY@users.noreply.github.com>
2024-09-02 14:07:35 +0300

    Update models.py

--- comments/models.py
+++ comments/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from django.conf import settings
 from articles.models import Article
 
@@ -10,3 +9,11 @@ class Comment(models.Model):
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_comments', blank=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.article}'
+    
+    @property
+    def like_count(self):
+        return self.likes.count()
