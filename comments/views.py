98871efd98f02ab74982e60333c82dd3970d406a
SpektorY <165672672+SpektorY@users.noreply.github.com>
2024-09-02 14:07:58 +0300

    Update views.py

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics , status
+from rest_framework import generics, status, views
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response 
@@ -18,10 +18,8 @@ class CommentView(generics.ListCreateAPIView):
             return [IsAuthenticatedOrReadOnly()]
         return super().get_permissions()
 
-    
     def post(self, request, slug, *args, **kwargs):
         try:
-            
             article = Article.objects.get(slug=slug)
             comment_data = request.data.get('comment')
             
@@ -32,56 +30,81 @@ class CommentView(generics.ListCreateAPIView):
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
         
-            return Response({ "comment": serializer.data } ,status=status.HTTP_200_OK)
+            return Response({ "comment": serializer.data } ,status=status.HTTP_201_CREATED)
             
-        except Exception:
-            return Response({"errors": {
-                "body": [
-                    "Bad Request"
-                ]
-            }}, status=status.HTTP_404_NOT_FOUND)
+        except Article.DoesNotExist:
+            return Response({"errors": {"body": ["Article not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"errors": {"body": ["Bad Request", str(e)]}}, status=status.HTTP_400_BAD_REQUEST)
             
-        
-    
     def list(self, request, slug, *args, **kwargs):
         try:
-        
             article = Article.objects.get(slug=slug)
             comments = Comment.objects.filter(article=article).order_by('-created')
-            
             serializer = self.get_serializer(comments, many=True)
+            response = {'comments': serializer.data}
+            return Response(response, status=status.HTTP_200_OK)
             
-            response = {
-                'comments' : serializer.data, 
-            }
-            
-            return Response(response)
-            
-        except Exception:
-            return Response({"errors": {
-                "body": [
-                    "Bad Request"
-                ]
-            }}, status=status.HTTP_404_NOT_FOUND)
-            
-        
-        
-    
+        except Article.DoesNotExist:
+            return Response({"errors": {"body": ["Article not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"errors": {"body": ["Bad Request", str(e)]}}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DeleteCommentView(generics.DestroyAPIView):
-    
+    permission_classes = [IsAuthenticated]
+
     def destroy(self, request, slug, id, *args, **kwargs):
         try:
-            
-            Article.objects.get(slug=slug)
+            article = Article.objects.get(slug=slug)
             comment = Comment.objects.get(id=id)
+            if comment.author != request.user:
+                return Response({"errors": {"body": ["You do not have permission to delete this comment."]}}, 
+                                status=status.HTTP_403_FORBIDDEN)
             self.perform_destroy(comment)
             return Response(status=status.HTTP_204_NO_CONTENT)
             
-        except Exception:
-            return Response({"errors": {
-                    "body": [
-                        "Bad Request"
-                    ]
-                }}, status=status.HTTP_404_NOT_FOUND)   
-            
+        except Article.DoesNotExist:
+            return Response({"errors": {"body": ["Article not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Comment.DoesNotExist:
+            return Response({"errors": {"body": ["Comment not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"errors": {"body": ["Bad Request", str(e)]}}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeCommentView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, slug, id, *args, **kwargs):
+        try:
+            article = Article.objects.get(slug=slug)
+            comment = Comment.objects.get(id=id, article=article)
+            if request.user in comment.likes.all():
+                return Response({"errors": {"body": ["You already liked this comment."]}}, 
+                                status=status.HTTP_400_BAD_REQUEST)
+            comment.likes.add(request.user)
+            return Response({"message": "Comment liked successfully."}, status=status.HTTP_200_OK)
+        
+        except Article.DoesNotExist:
+            return Response({"errors": {"body": ["Article not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Comment.DoesNotExist:
+            return Response({"errors": {"body": ["Comment not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"errors": {"body": ["Bad Request", str(e)]}}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, slug, id, *args, **kwargs):
+        try:
+            article = Article.objects.get(slug=slug)
+            comment = Comment.objects.get(id=id, article=article)
+            if request.user not in comment.likes.all():
+                return Response({"errors": {"body": ["You have not liked this comment."]}}, 
+                                status=status.HTTP_400_BAD_REQUEST)
+            comment.likes.remove(request.user)
+            return Response({"message": "Comment unliked successfully."}, status=status.HTTP_200_OK)
+        
+        except Article.DoesNotExist:
+            return Response({"errors": {"body": ["Article not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Comment.DoesNotExist:
+            return Response({"errors": {"body": ["Comment not found."]}}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"errors": {"body": ["Bad Request", str(e)]}}, status=status.HTTP_400_BAD_REQUEST)
