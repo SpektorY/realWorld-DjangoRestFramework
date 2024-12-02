b9e0b3a160fa0805396604f79e75d6342c85a87b
SpektorY <165672672+SpektorY@users.noreply.github.com>
2024-12-02 09:33:07 +0200

    Update serializers.py

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,31 +1,38 @@
 from rest_framework import serializers
-
 from comments.models import Comment
 from articles.serializers import AuthorSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SerializerMethodField()
-    createdAt = serializers.DateTimeField(source='created', format='%Y-%m-%dT%H:%M:%S.%fZ', required=False)
-    updatedAt = serializers.DateTimeField(source='updated', format='%Y-%m-%dT%H:%M:%S.%fZ', required=False)
+    created_at = serializers.DateTimeField(
+        source='created', 
+        format='%Y-%m-%dT%H:%M:%S.%fZ', 
+        required=False
+    )
+    updated_at = serializers.DateTimeField(
+        source='updated', 
+        format='%Y-%m-%dT%H:%M:%S.%fZ', 
+        required=False
+    )
     body = serializers.CharField(source='content', required=True)
-    
-    
+
     class Meta:
-        model = Comment 
-        fields = ['id', 'createdAt', 'updatedAt', 'body', 'author']
-        
-    
+        model = Comment
+        fields = ['id', 'created_at', 'updated_at', 'body', 'author']
+
     def get_author(self, obj):
         request = self.context.get('request')
-        serializer = AuthorSerializer(obj.author, context={'request': request})
-        return serializer.data
-    
+        return AuthorSerializer(obj.author, context={'request': request}).data
+
     def create(self, validated_data):
-        comment = Comment(
+        user = self.context['request'].user
+        article = self.context.get('article')
+        if not article:
+            raise serializers.ValidationError("Article context is required to create a comment.")
+        
+        return Comment.objects.create(
             **validated_data,
-            author=self.context['request'].user, 
-            article=self.context['article']
+            author=user,
+            article=article
         )
-        comment.save()
-        return comment
